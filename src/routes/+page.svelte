a41fc694d585c06f5e5c7a0c0e0bf0138fe52660
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2023-01-20 19:23:14 +0000

    weeeee

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,14 @@
-<script lang="ts">
-	import Login from '../lib/Login.svelte';
-</script>
-
-<head>
+<svelte:head>
 	<title>Svelte Boi</title>
-</head>
+</svelte:head>
 <div>
 	<h1 class="text-center font-bold text-lg">Welcome to SvelteKit</h1>
 	<p>
 		Visit <a href="https://kit.svelte.dev" class="link">kit.svelte.dev</a> to read the documentation
 	</p>
 
-	<Login />
+	<!-- <Login /> -->
 
 	<a href="/about" class="link">about</a>
+	<a href="/signup" class="link">Signup</a>
 </div>
---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import Login from "$lib/Login.svelte";
+</script>
+
+<head>
+    <title>Signup</title>
+</head>
+
+<body>
+    <Login />
+</body>
